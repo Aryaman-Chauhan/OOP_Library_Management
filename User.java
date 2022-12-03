96729bcffa00b16a66d79b967ba41e39b96dac3e
Manpreet <man.ahluwalia13@gmail.com>
2022-12-03 22:28:22 +0530

    Added getters in User.java

--- User.java
+++ User.java
@@ -2,6 +2,14 @@ public class User {
     protected String id;
     protected String password;
 
+    public String getID(){
+        return this.id;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
